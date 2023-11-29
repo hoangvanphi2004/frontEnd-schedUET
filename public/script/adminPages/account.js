311a7c311e5d39b44d060f072b5706456fe4c420
thanhdaonguyen <110709579+thanhdaonguyen@users.noreply.github.com>
2023-11-29 15:13:43 +0700

    add delete feature in account and course pages

--- public/script/adminPages/account.js
+++ public/script/adminPages/account.js
@@ -17,3 +17,30 @@ document.querySelectorAll("#icon_add_account").forEach(
 document.querySelector("#close_edit_account").onclick = () => {
   document.querySelector("#modal_1_account").classList.remove("active");
 };
+
+async function delete_account(element) {
+  let userDataRow = element.parentNode.parentNode;
+  let num = element.id[element.id.length - 1];
+  let userID = userDataRow.querySelector(`#userID-${num}`).textContent;
+  let password = userDataRow.querySelector(`#password-${num}`).textContent;
+  let isAdmin = userDataRow.querySelector(`#isAdmin-${num}`).textContent;
+
+  let data = {
+    userID: userID,
+    password: password,
+    isAdmin: isAdmin,
+  };
+
+  await axios
+    .delete(`http://localhost:8000/accounts/${userID}`)
+    .then((result) => {
+      location.reload();
+    })
+    .catch((err) => {
+      console.log("this is the error:");
+      console.log(err.response.data);
+      alert("Can't delete this account");
+    });
+}
+
+
