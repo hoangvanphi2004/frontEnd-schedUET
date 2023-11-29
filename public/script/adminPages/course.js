fa8fe4ab63b5c64e22737ba1c807ae4244127e73
thanhdaonguyen <110709579+thanhdaonguyen@users.noreply.github.com>
2023-11-29 22:28:29 +0700

    finish course page's features

--- public/script/adminPages/course.js
+++ public/script/adminPages/course.js
@@ -20,9 +20,17 @@ document.querySelector("#close_edit_course").onclick = () => {
 };
 
 
+//delete course
 async function delete_course(element) {
   let userDataRow = element.parentNode.parentNode;
-  let num = element.id[element.id.length - 1];
+  let indexOfDash = 0;
+  for (let i = element.id.length - 1; i >= 0; i--) {
+    if (element.id[i] == '_') {
+      indexOfDash = i;
+      break;
+    } 
+  }
+  let num = element.id.substring(indexOfDash + 1);
   let courseID = userDataRow.querySelector(`#courseID-${num}`).textContent;
   let title = userDataRow.querySelector(`#title-${num}`).textContent;
   let credits = userDataRow.querySelector(`#credits-${num}`).textContent;
@@ -37,5 +45,73 @@ async function delete_course(element) {
       console.log("this is the error:");
       console.log(err.response.data);
       alert("Can't delete this account");
+    });
+}
+
+//add course
+async function addData_Course() {
+  let courseID = document.querySelector("#courseID_addInput").value;
+  let title = document.querySelector("#title_addInput").value;
+  let credits = document.querySelector("#credits_addInput").value;
+
+  await axios
+    .post("http://localhost:8000/courses", {
+      courseID: courseID,
+      title: title,
+      credits: credits
+    })
+    .then((result) => {
+      location.reload();
+    })
+    .catch((err) => {
+      console.log("this is the error:");
+      console.log(err.response.data);
+      alert("Can't add this course");
+    });
+}
+
+//edit course
+let editedCourseID = 0;
+function openEditTab(element) {
+  let courseDataRow = element.parentNode.parentNode;
+  let indexOfDash = 0;
+  for (let i = element.id.length - 1; i >= 0; i--) {
+    if (element.id[i] == '_') {
+      indexOfDash = i;
+      break;
+    } 
+  }
+  let num = element.id.substring(indexOfDash + 1);
+  let courseID = courseDataRow.querySelector(`#courseID-${num}`).textContent;
+  let title = courseDataRow.querySelector(`#title-${num}`).textContent;
+  let credits = courseDataRow.querySelector(`#credits-${num}`).textContent;
+
+
+  document.querySelector("#courseID_editInput").value = courseID;
+  document.querySelector("#title_editInput").value = title;
+  document.querySelector("#credits_editInput").value = credits;
+  document.querySelector("#modal_1_course").classList.toggle("active");
+  
+  editedCourseID = courseID;
+}
+
+async function editData_Course() {
+  let courseID = document.querySelector("#courseID_editInput").value
+  let title = document.querySelector("#title_editInput").value
+  let credits = document.querySelector("#credits_editInput").value
+
+
+  await axios
+    .put(`http://localhost:8000/courses/${courseID}`, {
+      courseID: courseID,
+      title: title,
+      credits: credits
+    })
+    .then((result) => {
+      location.reload();
+    })
+    .catch((err) => {
+      console.log(err.response.data);
+      alert("Can't edit this teacher information");
     });
 }
