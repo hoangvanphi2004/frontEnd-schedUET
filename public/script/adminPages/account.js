702164d763333ea4461c846fea0f2a7e63497fd6
Thiện <125445860+nvt18624@users.noreply.github.com>
2023-12-09 14:35:28 +0700

    fix_frontEnd continue

--- public/script/adminPages/account.js
+++ public/script/adminPages/account.js
@@ -120,3 +120,25 @@ async function addData_Account() {
       alert("Can't delete this account");
     });
 }
+
+function myFunctionAccount() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput_account");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("account_table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
